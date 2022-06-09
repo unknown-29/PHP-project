9a8e184917c79ce7da40f82206579e10d48e16b2
unknown-29 <107143447+unknown-29@users.noreply.github.com>
2022-06-09 12:22:29 +0530

    Delete LoginSystem_CE047_CE066 directory

--- LoginSystem_CE047_CE066/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if(isset($_SESSION['name'])){
-	session_unset();
-	$msg="You have successfully logged out.";
-	header("Location:loginPage.php?msg=$msg");
-}
-?>
--- LoginSystem_CE047_CE066/signUp.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require_once "config.php";
-	$uname = base64_decode(urldecode($_GET["uname"]));
-	if(isset($_GET["signUp"])){
-		echo "HEY<br/>";
-	}
-	//$sql="INSERT INTO `users`(`username`, `password`) VALUES ($uname,'[value-2]','[value-3]')"
-?>
